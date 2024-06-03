d431c60421aee569bb1166bb721bf6e9f12af2dd
Nurislam <NurislamKhabibullin17@yandex.ru>
2024-06-03 17:14:02 +0500

    fix some line

--- src/GETTERandSETTER/PersonForGetter.java
+++ src/GETTERandSETTER/PersonForGetter.java
@@ -4,11 +4,22 @@ public class PersonForGetter {
     private String name;
     private int age;
 
-    public void setName(String userName) { name = userName; }
+    public void setName(String name) {
+        if (name.isEmpty()) {
+            System.out.println("Ввели пустую строку...");
+        } else {
+            this.name = name;
+        }
+    }
 
     public String getName() { return name; }
 
-    public void setAge(int userAge) { age = userAge; }
+    public void setAge(int age) {
+        if (age < 0 || age > 100) {
+            System.out.println("Проверьте ввод данных...");
+        } else {
+            this.age = age; }
+        }
 
     public int getAge() { return age; }
 }
